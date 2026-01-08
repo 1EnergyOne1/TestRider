b8cbb7f62fbbb61b6acdc1a336226ebcfab5d44f
energy <energone.tony@mail.ru>
2026-01-09 01:24:57 +0300

    ignore

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,8 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using ConsoleApp1;
+
+var input = 12;
+
+var sum = new Sum();
+var res = sum.Res(input);
---
+++ GymTestProjectCleanArc/GymManagement.Api/Program.cs
@@ -0,0 +1,29 @@
+using Microsoft.OpenApi.Models;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Добавляем сервисы в контейнер
+builder.Services.AddControllers();                   // Поддержка контроллеров
+builder.Services.AddEndpointsApiExplorer(); 
+// Конечные точки API Explorer
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new OpenApiInfo { Title = "My API", Version = "v1" });
+});                                                 // Генерация документации API (Swagger)
+
+var app = builder.Build();
+
+// Настройка HTTP-конвейера запросов
+if (app.Environment.IsDevelopment())                // Только для режима разработки
+{
+    app.UseSwagger();                                // Подключение Swagger UI
+    app.UseSwaggerUI(c =>                            // Настройка Swagger UI
+    {
+        c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1");
+    });
+}
+
+//app.UseHttpsRedirection();                          // Переадресация на HTTPS
+app.MapControllers();                               // Маршрутизация контроллеров
+
+app.Run();                                          // Запуск приложения
